1fc5e13e4bdc617a44df20526c285a622aeedec9
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-02 11:14:18 +0300

    Find the prime numbers

---
+++ prime-summation.py
@@ -0,0 +1,8 @@
+
+# returns True if parameter n is a prime number, False if composite and "Neither prime, nor composite" if neither
+def isPrime(n):
+    if n < 2: return "notprime"
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
