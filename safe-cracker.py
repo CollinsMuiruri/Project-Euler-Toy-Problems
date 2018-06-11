72cab32c8984451f1280ffbba6a48bbf4dfc6b5f
Collins Muiruri <wanyekicollins@gmail.com>
2018-06-11 23:32:47 +0300

    Update  toy problem

--- safe-cracker.py
+++ safe-cracker.py
@@ -1,13 +1,17 @@
 # When given a specific word, this function obtains all possibilities of the word
-# in an order
-# which starts from first letters eg: AAAA;
-# It then breaks when the specified word is found
+# in an order which starts from first letters eg: AAAA;
+# By giving the number equal to repeat, the function will give the length of the
+# string by the number given.
+# It then breaks when the specified word is found.
 # However, it will also give all combinations of a word if a "break" word is not given.
 
 from itertools import product
+import emoji
+
 
 # check permutations until we find the word 'crack'
-for x in product('123', repeat=3):
+for x in product('ARCK', repeat=5):
     w = ''.join(x)
     print(w)
     if w.lower() == 'crack': break
+    print(emoji.emojize(':+1:', use_aliases=True))
